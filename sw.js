8e44e1bfaa8a8f533efc57368196c20d8c4177b0
K41QU3 <162851465+bartendersempire@users.noreply.github.com>
2026-08-19 13:45:55 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',()=>{});
-self.addEventListener('message',e=>{
-  if(e.data?.type==='NOTIFY'){
-    self.registration.showNotification(e.data.title,{body:e.data.body,icon:'icon.svg',badge:'icon.svg'});
-  }
-});
